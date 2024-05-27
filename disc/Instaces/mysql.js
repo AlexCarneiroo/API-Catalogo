dd66da58823a8db6f426f7fdf92760d4a7ec4175
AlexCarneiroo <alexcarneiro139@gmail.com>
2024-05-27 19:36:12 -0300

    teste 2

--- disc/Instaces/mysql.js
+++ disc/Instaces/mysql.js
@@ -1,16 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-exports.sequelize = new sequelize_1.Sequelize('produtos', // Nome do banco de dados
-'root', // Nome do usuário
-'alex2003', // Senha
-{
+// Substitua esses valores pelos dados do seu banco de dados
+const dbName = 'produtos';
+const dbUser = 'root';
+const dbPassword = 'alex2003';
+const dbHost = 'mydbinstance.abcdefghijk.us-west-2.rds.amazonaws.com'; // Substitua pelo endpoint da sua instância RDS
+const dbPort = 3306; // Porta padrão do MySQL
+exports.sequelize = new sequelize_1.Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
     dialect: 'mysql',
-    port: 3306 // Porta padrão do MySQL
+    port: dbPort
+});
+// Testar a conexão
+exports.sequelize.authenticate()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+    .catch(err => {
+    console.error('Unable to connect to the database:', err);
 });
--- src/Instaces/mysql.ts
+++ src/Instaces/mysql.ts
@@ -1,15 +1,28 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-import dotenv from 'dotenv'
-
-dotenv.config()
+// Substitua esses valores pelos dados do seu banco de dados
+const dbName = 'produtos';
+const dbUser = 'root';
+const dbPassword = 'alex2003';
+const dbHost = 'mydbinstance.abcdefghijk.us-west-2.rds.amazonaws.com'; // Substitua pelo endpoint da sua instância RDS
+const dbPort = 3306; // Porta padrão do MySQL
 
 export const sequelize = new Sequelize(
-    'produtos', // Nome do banco de dados
-    'root',      // Nome do usuário
-    'alex2003',  // Senha
-    {
-        dialect: 'mysql',
-        port: 3306 // Porta padrão do MySQL
-    }
+  dbName,
+  dbUser,
+  dbPassword,
+  {
+    host: dbHost,
+    dialect: 'mysql',
+    port: dbPort
+  }
 );
+
+// Testar a conexão
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
