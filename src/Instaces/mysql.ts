f62aaab79a36db409d91c8fab0391c605f7e8321
AlexCarneiroo <alexcarneiro139@gmail.com>
2024-05-28 12:46:52 -0300

    teste3

--- src/Instaces/mysql.ts
+++ src/Instaces/mysql.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 // Substitua pelos valores reais do seu banco de dados RDS
-const dbName = 'produtos';
-const dbUser = 'root';
+const dbName = 'catalogo';
+const dbUser = 'admin';
 const dbPassword = 'alex2003';
 const dbHost = 'catalogo.c164u6c64wo7.us-east-1.rds.amazonaws.com'; // Endpoint correto da instância RDS
 const dbPort = 3306; // Porta padrão do MySQL
